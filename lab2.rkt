02107d321745e097aa1c90acf4e36fc76b9145c5
EnderJam07 <samjamham07@gmail.com>
2026-04-21 11:26:23 -0700

    Add files via upload

--- lab2.rkt
+++ lab2.rkt
@@ -1,19 +1,28 @@
 #lang racket
-(require htdp/testing)
-(require racket/trace)
-(define (mystery L)
-(if (null? L)
-L
-(append (mystery (cdr L))
-(list (car L)))))
-(check-expect (mystery '(1 2 3)) '(3 2 1))
-(check-expect (mystery '((1 2) (3 4) 5 6)) '(6 5 (3 4) (1 2)))
-(trace mystery)
-(generate-report)
-
+;1.
 (define (gen-list start end)
-  (if (> start end)
-      '()
-      (cons start (gen-list (+ start 1) end))))
-
-(gen-list 20 10)
+  (cond
+    [(> start end) '()]
+    [else (cons start (gen-list (+ start 1) end))]))
+;2.
+(define (sum L)
+  (cond
+    [(null? L) 0]
+    [else (+ (car L) (sum (cdr L)))]))
+;3.
+(define (retrieve-first-n n L)
+  (cond
+    [(> n (length L)) L]
+    [(<= n 0) '()]
+    [else (cons (car L) (retrieve-first-n (- n 1) (cdr L)))]))
+;4.
+(define (pair-sum? L n)
+  (cond
+    [(< (length L) 2) #f]
+    [(= (+ (car L) (car (cdr L))) n) #t]
+    [else (pair-sum? (cdr L) n)]))
+;4.
+(define (mystery-tail L [acc '()])
+  (cond
+      [(null? L) acc]
+      [else (mystery-tail (cdr L) (append (list (car L)) acc))]))
