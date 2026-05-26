7478a16601529128a1c1158a2736b3131d8ac548
EnderJam07 <samjamham07@gmail.com>
2026-05-26 12:24:30 -0700

    Add files via upload

--- lab7.rkt
+++ lab7.rkt
@@ -5,14 +5,6 @@
     [(null? L) #f]
     [(equal? (first L) x) #t]
     [else (member? x (cdr L))]))
-
-(define (intersect S1 S2 [acc '()])
-  (cond
-    [(null? S1) acc]
-    [(null? S2) acc]
-    [(member? (car S1) S2) (intersect (cdr S1) S2 (append acc (list (car S1))))]
-    [else (intersect (cdr S1) S2) acc]))
-
 (define (check-pairs x yz L)
   (cond
     [(null? yz) #t]
@@ -26,15 +18,13 @@
 (define (build-new-pairs x yz-pairs)
   (cond
     [(null? yz-pairs) '()]
-    [else (cons (list x (cadar yz-pairs))
-                (build-new-pairs x (cdr yz-pairs)))]))
+    [else (cons (list x (cadar yz-pairs)) (build-new-pairs x (cdr yz-pairs)))]))
 ;1.
 (define (Reflexive-Closure L S [full L])
   (cond
     [(null? S) full]
     [(not (member? (list (car S) (car S)) L)) (Reflexive-Closure L (cdr S) (append full (list (list (car S) (car S)))))]
     [else (Reflexive-Closure L (cdr S) full)]))
-
 ;2.
 (define (Symmetric-Closure L [full L])
   (cond
